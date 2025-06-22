dd0ad390f704e40d1d42ba487b9ed2b468541824
Carl Marcum <carl.marcum@codebuilders.net>
2025-06-22 14:24:01 -0400

    followup commit to get project to bootRun with testcontainers and postgresql.

---
+++ grails-app/init/net/codebuilders/mybusiness/BootStrap.groovy
@@ -0,0 +1,15 @@
+package net.codebuilders.mybusiness
+
+import jakarta.servlet.ServletContext
+
+class BootStrap {
+
+    ServletContext servletContext
+
+    def init = {
+    }
+
+    def destroy = {
+    }
+
+}
